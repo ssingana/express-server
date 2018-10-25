f12b202dacbc4611c9fbfe50af270eb077ee0e45
Singana Sreenivasulu (RBEI/BSP2) <Sreenivasulu.Singana@in.bosch.com>
2018-10-25 15:12:08 +0530

    express server

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # express-server
 Setting up the express server
+
+git clone server-express project.
+npm install
+node server
--- server.js
+++ server.js
@@ -2,26 +2,26 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const PORT = 3000;
-const app = express();
-app.use(bodyParser.json());
-app.use(cors());
+const SERVER_PORT = 3000;
+const expressObject = express();
+expressObject.use(bodyParser.json());
+expressObject.use(cors());
 
-app.get('/', function(req,res){
+expressObject.get('/', function(req,res){
     res.send('Hello from express server');
 });
 
-app.listen(PORT, function(){
-    console.log("Express server running on localhost :" + PORT);
+expressObject.listen(SERVER_PORT, function(){
+    console.log("Express server running on localhost :" + SERVER_PORT);
 });
 
-app.post('/user', function(req,res){
+expressObject.post('/user', function(req,res){
     console.log('Posting the user data started.');
     console.log(req.body);
     res.status(200).send({"message": "Data successfully received."});
     console.log('Posting the user data ends.');
 });
 
-app.post('/error', function(req,res){
+expressObject.post('/error', function(req,res){
     res.status(401).send({"message": "Posting failed."});
 });
